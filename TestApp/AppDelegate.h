4ff77c6844aa6a1f2933dcdb7e53de83a4ed5448
macintosh <sumenhhoangkim@gmail.com>
2017-06-10 19:46:00 +0700

    first commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Macintosh on 6/8/17.
+//  Copyright © 2017 Macintosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Macintosh on 6/8/17.
+//  Copyright © 2017 Macintosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
